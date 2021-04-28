770fe3b0e410174bff92c101dd1a089ca1618a6a
Chris Child <chris.child@gmail.com>
2021-04-28 10:36:09 +0100

    content provider class

---
+++ src/TessApi/ContentProvider.ts
@@ -0,0 +1,27 @@
+import { Content } from './Content';
+
+class ContentProvider extends Content {
+  title: string;
+  image_url: string;
+  description: string;
+  content_provider_type: any;
+  node_name: string;
+  keywords: Array<string>;
+  content_provider: any;
+
+  ///TODO: change type
+  constructor(provider: any) {
+    super();
+    this._base = `${this._base}/content_providers`;
+
+    this.title = provider.title;
+    this.url = provider.url;
+    this.image_url = provider.image_url;
+    this.description = provider.description;
+    this.content_provider_type = provider.content_provider_type;
+    this.node_name = provider.node_name;
+    this.keywords = provider.keywords;
+  }
+}
+
+export { ContentProvider };
